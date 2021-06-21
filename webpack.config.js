adfb4c982a9bc93b2676e09aac2aca9250435310
Matt Wills <mattwillscode@gmail.com>
2021-06-21 08:41:43 -0400

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/script.js',
+    module: {
+        rules: [
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+            { test: /\.(js)$/, use: 'babel-loader' },
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+}
